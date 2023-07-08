d9cfca846ed215e09caa06691852e89ac84b3adb
Sameer21x <109617908+Sameer21x@users.noreply.github.com>
2023-07-08 10:16:14 +0500

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,314 @@
+import 'package:flutter/material.dart';
+
+
+
+class Recipe {
+  String label;
+  String imageUrl;
+  int servings;
+  List<Ingredient> ingredients;
+  
+  static List<Recipe> samples=[    Recipe(
+      'Spaghetti and Meatballs',
+      'assets/2126711929_ef763de2b3_w.jpg',
+      4,
+      [
+        Ingredient(
+          1,
+          'box',
+          'Spaghetti',
+        ),
+        Ingredient(
+          4,
+          '',
+          'Frozen Meatballs',
+        ),
+        Ingredient(
+          0.5,
+          'jar',
+          'sauce',
+        ),
+      ],
+    ),
+    Recipe(
+      'Tomato Soup',
+      'assets/27729023535_a57606c1be.jpg',
+      2,
+      [
+        Ingredient(
+          1,
+          'can',
+          'Tomato Soup',
+        ),
+      ],
+    ),
+    Recipe(
+      'Grilled Cheese',
+      'assets/3187380632_5056654a19_b.jpg',
+      1,
+      [
+        Ingredient(
+          2,
+          'slices',
+          'Cheese',
+        ),
+        Ingredient(
+          2,
+          'slices',
+          'Bread',
+        ),
+      ],
+    ),
+    Recipe(
+      'Chocolate Chip Cookies',
+      'assets/15992102771_b92f4cc00a_b.jpg',
+      24,
+      [
+        Ingredient(
+          4,
+          'cups',
+          'flour',
+        ),
+        Ingredient(
+          2,
+          'cups',
+          'sugar',
+        ),
+        Ingredient(
+          0.5,
+          'cups',
+          'chocolate chips',
+        ),
+      ],
+    ),
+    Recipe(
+      'Taco Salad',
+      'assets/8533381643_a31a99e8a6_c.jpg',
+      1,
+      [
+        Ingredient(
+          4,
+          'oz',
+          'nachos',
+        ),
+        Ingredient(
+          3,
+          'oz',
+          'taco meat',
+        ),
+        Ingredient(
+          0.5,
+          'cup',
+          'cheese',
+        ),
+        Ingredient(
+          0.25,
+          'cup',
+          'chopped tomatoes',
+        ),
+      ],
+    ),
+    Recipe(
+      'Hawaiian Pizza',
+      'assets/15452035777_294cefced5_c.jpg',
+      4,
+      [
+        Ingredient(
+          1,
+          'item',
+          'pizza',
+        ),
+        Ingredient(
+          1,
+          'cup',
+          'pineapple',
+        ),
+        Ingredient(
+          8,
+          'oz',
+          'ham',
+        ),
+      ],
+    ),
+  ];
+
+  
+    Recipe(
+    this.label,
+    this.imageUrl,
+    this.servings,
+    this.ingredients,
+  );
+}
+  
+
+
+class Ingredient {
+  double quantity;
+  String measure;
+  String name;
+
+  Ingredient(
+    this.quantity,
+    this.measure,
+    this.name,
+  );
+}
+
+class RecipeApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Recipe App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Recipe App'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  final String title;
+
+  MyHomePage({required this.title});
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List<Recipe> recipes = Recipe.samples;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: SafeArea(
+        child: ListView.builder(
+          itemCount: recipes.length,
+          itemBuilder: (BuildContext context, int index) {
+            return GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => RecipeDetailsScreen(recipe: recipes[index]),
+                  ),
+                );
+              },
+              child: buildRecipeCard(recipes[index]),
+            );
+          },
+        ),
+      ),
+    );
+  }
+
+  Widget buildRecipeCard(Recipe recipe) {
+    return Card(
+      elevation: 2.0,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: <Widget>[
+            Image.asset(
+              recipe.imageUrl,
+              height: 200,
+              width: double.infinity,
+              fit: BoxFit.cover,
+            ),
+            const SizedBox(
+              height: 14.0,
+            ),
+            Text(
+              recipe.label,
+              style: const TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.w700,
+                fontFamily: 'Palatino',
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class RecipeDetailsScreen extends StatefulWidget {
+  final Recipe recipe;
+
+  RecipeDetailsScreen({required this.recipe});
+
+  @override
+  _RecipeDetailsScreenState createState() => _RecipeDetailsScreenState();
+}
+
+class _RecipeDetailsScreenState extends State<RecipeDetailsScreen> {
+  int _sliderVal = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.recipe.label),
+      ),
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[
+            SizedBox(
+              height: 300,
+              width: double.infinity,
+              child: Image.asset(
+                widget.recipe.imageUrl,
+              ),
+            ),
+            const SizedBox(
+              height: 4,
+            ),
+            Text(
+              'Ingredients:',
+              style: const TextStyle(fontSize: 18),
+            ),
+            Expanded(
+              child: ListView.builder(
+                padding: const EdgeInsets.all(7.0),
+                itemCount: widget.recipe.ingredients.length,
+                itemBuilder: (BuildContext context, int index) {
+                  final ingredient = widget.recipe.ingredients[index];
+                  return ListTile(
+                    title: Text('${ingredient.quantity} ${ingredient.measure} ${ingredient.name}'),
+                  );
+                },
+              ),
+            ),
+            Slider(
+              min: 1,
+              max: 10,
+              divisions: 9,
+              label: 'Servings: ${_sliderVal.toInt()}',
+              value: _sliderVal.toDouble(),
+              onChanged: (newValue) {
+                setState(() {
+                  _sliderVal = newValue.round();
+                });
+              },
+              activeColor: Colors.green,
+              inactiveColor: Colors.black,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(RecipeApp());
+}
